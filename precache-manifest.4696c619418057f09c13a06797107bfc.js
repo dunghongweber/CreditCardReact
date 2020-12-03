d98ecbf74e9045baecf131afae4775a772a26fa6
dunghongweber <39318997+dunghongweber@users.noreply.github.com>
2020-12-02 17:53:20 -0700

    Updates

---
+++ precache-manifest.4696c619418057f09c13a06797107bfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ac7dc579e5c5e2f791c0b47aa06f66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "550b33319028a3506a92",
+    "url": "/static/css/main.28958d48.chunk.css"
+  },
+  {
+    "revision": "f88c72fdf7269c073438",
+    "url": "/static/js/2.afb6133b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.afb6133b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "550b33319028a3506a92",
+    "url": "/static/js/main.14b71b31.chunk.js"
+  },
+  {
+    "revision": "06167012e7f93ac03bfd",
+    "url": "/static/js/runtime-main.9817be0e.js"
+  }
+]);
